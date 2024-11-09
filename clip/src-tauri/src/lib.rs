edb426b0bcc705ca0eca2f237006e247f0e16f35
akash2061 <lucifermorningstar2061@gmail.com>
2024-11-09 18:21:17 +0530

    Clipboard example

---
+++ clip/src-tauri/src/lib.rs
@@ -0,0 +1,21 @@
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use tauri_plugin_clipboard_manager::ClipboardExt;
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_clipboard_manager::init())
+        .setup(|app| {
+            app.clipboard().write_text("Tauri is awesome!")?;
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
